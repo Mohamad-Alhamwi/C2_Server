1342d5d7eadd7877319bebe9f132becece277ac0
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2024-12-18 22:38:39 +0300

    Update agent handler.

--- include/agent_handler.h
+++ include/agent_handler.h
@@ -7,15 +7,16 @@
 /* Define the Agent structure */
 typedef struct
 {
-    int agent_sock_fd;                      // Agent's socket file descriptor.
-    struct sockaddr_in agent_addr;          // Agent's address (IP, port).
-    char agent_data_buff [1024];            // Buffer for communication.
+    int sock_fd;                      // Agent's socket file descriptor.
+    struct sockaddr_in addr;          // Agent's address (IP, port).
+    socklen_t addr_len;               // Agent's address length.
+    char data_buff [1024];            // Buffer for communication.
 } Agent;
 
 /* Agent handler functions */
-void initAgent(Agent *agent, int sock_fd, struct sockaddr_in *addr);
+void initAgent(Agent *agent, int sock_fd, struct sockaddr_in *addr, socklen_t addr_len);
 ssize_t receiveAgentData(Agent *agent, int flags);
-ssize_t sendAgentData(Agent *agent, const void *msg, size_t len, int flags);
+ssize_t sendDataToAgent(Agent *agent, const void *msg, size_t msg_len, int flags);
 void closeAgent(Agent *agent);
 
 # endif
--- src/agent_handler.c
+++ src/agent_handler.c
@@ -2,36 +2,39 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
 /* Initialize a new agent structure. */
-void initAgent(Agent *agent, int sock_fd, struct sockaddr_in *addr);
+void initAgent(Agent *agent, int sock_fd, struct sockaddr_in *addr, socklen_t addr_len)
 {
-    agent -> agent_sock_fd = sock_fd;
-    agent -> agent_addr = *addr;
-    memset(agent -> agent_data_buff, 0, sizeof(agent -> agent_data_buff));  // Clear the buffer.
+    agent -> sock_fd = sock_fd;
+    agent -> addr = *addr;
+    agent -> addr_len = addr_len; // TODO: Needs to be changed, as a pointer or as a value?
+    memset(agent -> data_buff, 0, sizeof(agent -> data_buff));  // Clear the buffer.
 }
 
 /* Receive data from agent. */
-ssize_t receiveAgentData(Agent *agent, int flags);
+ssize_t receiveAgentData(Agent *agent, int flags)
 {
-    ssize_t bytes_received = recv(agent -> agent_sock_fd, agent -> agent_data_buff, sizeof(agent -> agent_data_buff), flags);
+    ssize_t bytes_received = recv(agent -> sock_fd, agent -> data_buff, sizeof(agent -> data_buff), flags);
 
     if (bytes_received > 0)
     {
-        printf("Received from %s:%d: %.*s\n", inet_ntoa(agent -> addr.sin_addr), ntohs(agent -> addr.sin_port), (int)bytes_received, agent -> agent_data_buff);
+        printf("Received from %s:%d: %.*s\n", inet_ntoa(agent -> addr.sin_addr), ntohs(agent -> addr.sin_port), (int)bytes_received, agent -> data_buff);
     }
+
     return bytes_received;
 }
 
 /* Send response to agent. */
-ssize_t sendAgentData(Agent *agent, const void *msg, size_t msg_len, int flags);
+ssize_t sendDataToAgent(Agent *agent, const void *msg, size_t msg_len, int flags)
 {
-    return send(agent -> agent_sock_fd, msg, msg_len, flags);
+    return send(agent -> sock_fd, msg, msg_len, flags);
 }
 
 /* Close agent connection. */
 void closeAgent(Agent *agent)
 {
-    close(agent -> agent_sock_fd);
+    close(agent -> sock_fd);
     printf("Closed connection with %s:%d\n", inet_ntoa(agent -> addr.sin_addr), ntohs(agent -> addr.sin_port));
 }
