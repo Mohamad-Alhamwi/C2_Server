a3b8bcc25f4b2a847d2cbed3e31eb901a5bd7ffd
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2024-12-19 20:52:20 +0300

    Update throwError().
    Added a condition for the abortion case.

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void throwError(const char *custom_err_msg);
+void throwError(const char *custom_err_msg, int should_abort);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -4,11 +4,23 @@
 #include <errno.h>
 #include <string.h>
 
-void throwError(const char *custom_err_msg)
+void throwError(const char *custom_err_msg, int should_abort)
 {
+    /*Handle errors that occur in a system call or library function.*/
     fprintf(stderr, "%s.\n", custom_err_msg);
+    fflush(stderr);  // Ensure the custom error message is printed.
     fprintf(stderr, "Error Number: %d.\n", errno);
+    fflush(stderr);  // Ensure error number is printed.
     fprintf(stderr, "Error Description: %s.\n", strerror(errno));
-    fprintf(stderr, "Aborted.\n");
-    exit(EXIT_FAILURE);
+    fflush(stderr);  // Ensure error description is printed.
+
+    if(should_abort != 0)
+    {
+        fprintf(stderr, "Aborted.\n");
+        fflush(stderr);  // Ensure the abort message is printed.
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: Handle custom errors.
+    return;
 }
