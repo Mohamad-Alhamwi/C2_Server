402c689ab7587ffe40aad7a2a8a8f682a779fabf
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2024-12-25 20:16:06 +0300

    Add macros.

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,11 @@
 # ifndef UTILS_H
 # define UTILS_H
 
+# define FALSE 0
+# define TRUE 1
+# define CLEANUP_FILE_DESCRIPTORS 1
+# define CLEANUP_MEMORY  2
+
 void abortOperation();
 void throwError(const char *custom_err_msg, int should_abort);
 void cleanUpResources(int cleanup_type, void *resource);
