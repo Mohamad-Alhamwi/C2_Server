fb9b155380cf2b6c49f8bb9ea471d49807281a7f
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2024-12-25 20:05:03 +0300

    Update main().

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
-    startServer(8888);
+    int backlog = 5; // The maximum length to which the queue of pending connections may grow.
+    int port = 8888;
+
+    startServer(port, backlog);
+    // TODO: check for invalid port numbers.
+    // TODO: take backlog and port number information from CLI.
     return 0;
 }
