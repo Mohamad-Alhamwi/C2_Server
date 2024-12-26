347e55b9eb2f952e3b678b19199028467de4ae52
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2024-12-26 15:18:31 +0300

    Update main().
    Check for command line arguments.

--- main.c
+++ main.c
@@ -1,12 +1,45 @@
-#include "server.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include "server.h"
+# include "utils.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int backlog = 5; // The maximum length to which the queue of pending connections may grow.
-    int port = 8888;
+    int port = 8888; // Default port.
+    int backlog = 5; // Default backlog.
 
+    // Parse command-line arguments for port and backlog.
+    if(argc == 1)
+    {
+        printf("Starting server with default values:\n");
+        printf("    Port: %d.\n    Backlog: %d.\n\n", port, backlog);
+        printf("Usage: %s <PORT> <BACKLOG>:\n", argv[0]);
+        printf("    <PORT>: The port on which the server will be listening for incoming connections.\n");
+        printf("    <BACKLOG>: The maximum length to which the queue of pending connections may grow.\n\n");
+    }
+
+    if(argc >= 2)
+    {
+        port = atoi(argv[1]);  // Convert first argument to integer (port).
+
+        if(port <= 0 || port > 65535)
+        {
+            throwError("Invalid port number. Please provide a port number between 1 and 65535", TRUE);
+        }
+    }
+
+    if(argc >= 3)
+    {
+        backlog = atoi(argv[2]);  // Convert second argument to integer (backlog).
+
+        if(backlog <= 0)
+        {
+            throwError("Invalid backlog value. Please provide a positive integer", TRUE);
+        }
+    }
+
+    // Start the server.
     startServer(port, backlog);
-    // TODO: check for invalid port numbers.
-    // TODO: take backlog and port number information from CLI.
+
     return 0;
 }
