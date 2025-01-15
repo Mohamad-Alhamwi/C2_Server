d45f5a34276272f5036c0d88b5f3b343e7cf0382
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-01-15 13:26:02 +0300

    Edit.

--- include/agent/server_handler.h
+++ include/agent/server_handler.h
@@ -1,7 +1,10 @@
-# ifndef SERVER_HANDLER_H
-# define SERVER_HANDLER_H
+#ifndef SERVER_HANDLER_H
+#define SERVER_HANDLER_H
 
-ssize_t receiveServerData(int sock_fd, const char *data_buff, size_t data_buff_size, int flags);
+#include <stddef.h>
+#include <sys/types.h>
+
+ssize_t receiveServerData(int sock_fd, char *data_buff, size_t data_buff_size, int flags);
 ssize_t sendDataToServer(int sock_fd, const void *msg, size_t msg_len, int flags);
 
-# endif
+#endif
