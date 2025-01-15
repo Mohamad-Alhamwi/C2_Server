7063c9296ba38d884bdf577a7fdf6800f3044d15
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-01-15 13:27:50 +0300

    Edit.

--- src/agent/server_handler.c
+++ src/agent/server_handler.c
@@ -1,16 +1,16 @@
+#include <stdio.h>
+#include <arpa/inet.h>
 
-# include "agent/server_handler.h"
-# include <stddef.h>
+#include "agent/server_handler.h"
 
 /* Receive data from server. */
-ssize_t receiveServerData(int sock_fd, const char *data_buff, size_t data_buff_size, int flags)
+ssize_t receiveServerData(int sock_fd, char *data_buff, size_t data_buff_size, int flags)
 {
     ssize_t bytes_received = recv(sock_fd, data_buff, sizeof(data_buff), flags);
 
     if (bytes_received > 0)
     {
-        // TODO: Why type casting (int)bytes_received???
-        printf("Received (%zd bytes) from %s:%d\n", bytes_received, inet_ntoa(agent -> addr.sin_addr), ntohs(agent -> addr.sin_port));
+        printf("Received (%zd bytes) from server\n", bytes_received);
     }
 
     return bytes_received;
