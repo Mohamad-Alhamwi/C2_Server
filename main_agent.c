abb523ae84fb651f1f7b35c8bc9048c37c1eea13
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-01-16 23:35:53 +0300

    Edit.

--- main_agent.c
+++ main_agent.c
@@ -2,9 +2,30 @@
 # include <stdlib.h>
 
 # include "shared/utils.h"
-# include "server/server.h"
+# include "agent/agent.h"
+
+void printUsageInformation(const char *program_name)
+{
+    printf("\n*************************************************************************************************\n");
+    printf("* " INFORMATIONAL "[*] " RESET "Usage: %s <PORT> <SERVER_ADDRESS>\t\t\t\t\t\t\t*\n", program_name);
+    printf("*************************************************************************************************\n");
+
+    return;
+}
 
 int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        printUsageInformation(argv[0]);
+
+        return 1;
+    }
+
+    /* TODO: Add parsing functionality for arguments. */
+    const char *server_ip = argv[1];
+    int server_port = atoi(argv[2]);
+    startAgent(server_ip, server_port);
+
     return 0;
 }
