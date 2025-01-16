0bebf3a8e3fa6c85d194838c1dd0605589fd8127
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-01-16 23:34:19 +0300

    Edit receiveServerData().

--- src/agent/server_handler.c
+++ src/agent/server_handler.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
+#include "shared/utils.h"
 #include "agent/server_handler.h"
 
 /* Receive data from server. */
 ssize_t receiveServerData(int sock_fd, char *data_buff, size_t data_buff_size, int flags)
 {
-    ssize_t bytes_received = recv(sock_fd, data_buff, sizeof(data_buff), flags);
+    char time_buff[DATE_TIME_BUFFER_SIZE];
+    ssize_t bytes_received = recv(sock_fd, data_buff, data_buff_size, flags);
 
     if (bytes_received > 0)
     {
-        printf("Received (%zd bytes) from server\n", bytes_received);
+        getTime(time_buff, sizeof(time_buff));
+        printf("[" INFORMATIONAL "%s" RESET "] " "[" SUCCESSFUL "+" RESET "] " "Received (%zd bytes) from the server\n", time_buff, bytes_received);
+        printf("[" INFORMATIONAL "%s" RESET "] " "[" SUCCESSFUL "+" RESET "] " "Received from the server: %s\n", time_buff, data_buff);
     }
 
     return bytes_received;
