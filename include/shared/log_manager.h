3dc3673d2f8211ae00b587fdeb5ce5aff15acc2e
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-02-15 16:20:07 +0300

    Add new files.
    Added log_manager() to handle logging.

---
+++ include/shared/log_manager.h
@@ -0,0 +1,23 @@
+#ifndef LOG_MANAGER_H
+#define LOG_MANAGER_H
+
+// Color codes.
+#define RESET         "\033[0m"      // Reset to default color.
+#define INFORMATIONAL "\033[34m"     // Blue for informational messages.
+#define SUCCESSFUL    "\033[32m"     // Green for success messages.
+#define ERROR         "\033[31m"     // Red for error messages.
+#define WARNING       "\033[33m"     // Yellow for warning messages.
+
+// Log message type's switch.
+#define LOG_INFORMATIONAL 0
+#define LOG_SUCCESSFUL    1
+#define LOG_WARNING       2
+#define LOG_ERROR         3
+
+// Log file's name.
+#define LOG_FILE "server_logs.txt"
+
+void logTerminal();
+void logFile();
+
+#endif
---
+++ src/shared/log_manager.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stddef.h> 
+#include <stdarg.h>
+
+#include "shared/time.h"
+#include "shared/log_manager.h"
+
+void logTerminal(size_t log_type, const char *log_msg_format, ...)
+{
+    char time_buff[TIMESTAMP_BUFFER_SIZE];
+    getTimestamp(time_buff, FORMAT_FULL_TIMESTAMP);
+
+    // Choose the correct output stream.
+    FILE *stream = (log_type == LOG_ERROR) ? stderr : stdout;
+
+    va_list var_arg_list;
+    va_start(var_arg_list, log_msg_format);
+
+    switch (log_type)
+    {
+        case LOG_INFORMATIONAL:
+            fprintf(stream, "[" INFORMATIONAL "%s" RESET "] " "[" INFORMATIONAL "*" RESET "] ", time_buff);
+            break;
+        
+        case LOG_SUCCESSFUL:
+            fprintf(stream, "[" INFORMATIONAL "%s" RESET "] " "[" SUCCESSFUL "+" RESET "] ", time_buff);
+            break;
+
+        case LOG_WARNING:
+            fprintf(stream, "[" INFORMATIONAL "%s" RESET "] " "[" WARNING "!" RESET "] ", time_buff);
+            break;
+        
+        case LOG_ERROR:
+            fprintf(stream, "[" INFORMATIONAL "%s" RESET "] " "[" ERROR "-" RESET "] ", time_buff);
+            break;
+        
+        default:
+            fprintf(stream, "[" INFORMATIONAL "%s" RESET "] " "[" WARNING "!" RESET "] " WARNING "Undefined log type! " RESET, time_buff);
+            break;
+    }
+
+    // Print the formatted message.
+    vfprintf(stream, log_msg_format, var_arg_list);
+    va_end(var_arg_list);
+
+    // Add newline for readability.
+    fprintf(stream, "\n");
+
+    // Ensure immediate output (useful for debugging).
+    fflush(stream);
+    
+    return;
+}
