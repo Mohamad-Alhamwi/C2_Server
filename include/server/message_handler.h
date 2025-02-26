b6f783c24297209bfc8ade4faa910b309fc471b7
Mohamad-Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-02-26 14:19:44 +0300

    Delete unused function
    Deleted the formatMessage() function.

--- include/server/message_handler.h
+++ include/server/message_handler.h
@@ -1,12 +1,10 @@
 #ifndef MESSAGE_HANDLER_H
 #define MESSAGE_HANDLER_H
 
-#include <stddef.h>
+#include <stdio.h>
 
-#define MSG_BUFF_SIZE 4096
 #define MSG_ID_SIZE 32
 
 void generateMessageID(char *msg_id);
-void formatMessage(char *buffer, const char *type, const char *action, const char *data);
 
 #endif
